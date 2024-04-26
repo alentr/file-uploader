220027c8855a71c0517765d25e304023f439e8ec
Alexandre Telles <alexandre.ntr@gmail.com>
2024-04-26 14:14:56 +0000

    upload file

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SingleFileUploader from './components/SingleFileUploader';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>File Upload</h1>
+
+      <h2>Single File Upload</h2>
+      <SingleFileUploader />
+    </>
   );
 }
 
